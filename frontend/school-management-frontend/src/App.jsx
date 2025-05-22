e6c2be8c954496281c88cbb6d43d37f9a3ec79a6
fayee-tokyo <faithmusila2004@gmail.com>
2025-05-22 11:36:13 +0300

    created backend and frontend folders

--- frontend/school-management-frontend/src/App.jsx
+++ frontend/school-management-frontend/src/App.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import About from './pages/About';
+import Navigationbar from './components/Navigationbar';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <>
+      <Navigationbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
   );
 }
+export default App
 
-export default App;
